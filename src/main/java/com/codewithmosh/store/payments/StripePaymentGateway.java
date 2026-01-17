a7feaf1abd14b959b9f821073f750cef2c1e2263
Bowen Zhang <bzhang@dalet.com>
2026-01-17 22:12:14 +1100

    Populating database

--- src/main/java/com/codewithmosh/store/payments/StripePaymentGateway.java
+++ src/main/java/com/codewithmosh/store/payments/StripePaymentGateway.java
@@ -33,7 +33,7 @@ public class StripePaymentGateway implements PaymentGateway {
                     .setMode(SessionCreateParams.Mode.PAYMENT)
                     .setSuccessUrl(websiteUrl + "/checkout-success?orderId=" + order.getId())
                     .setCancelUrl(websiteUrl + "/checkout-cancel")
-                    .putMetadata("order_id", order.getId().toString());
+                    .setPaymentIntentData(createPaymentIntentData(order));
 
             order.getItems().forEach(item -> {
                 var lineItem = createLineItem(item);
@@ -47,6 +47,12 @@ public class StripePaymentGateway implements PaymentGateway {
             System.out.println(ex.getMessage());
             throw new PaymentException();
         }
+    }
+
+    private static SessionCreateParams.PaymentIntentData createPaymentIntentData(Order order) {
+        return SessionCreateParams.PaymentIntentData.builder()
+                .putMetadata("order_id", order.getId().toString())
+                .build();
     }
 
     private SessionCreateParams.LineItem createLineItem(OrderItem item) {
